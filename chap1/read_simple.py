85ab5a980a2bd3f7a0e2df957904cfdadbe0f010
Nei Santos <ns@amp.co>
2017-10-27 12:45:04 +0100

    Simple read from mongo

---
+++ chap1/read_simple.py
@@ -0,0 +1,15 @@
+import bottle
+import pymongo
+
+@bottle.route('/')
+def index():
+	connection = pymongo.MongoClient('localhost', 27018)
+
+	db = connection.test
+	names = db.names
+	item = names.find_one()
+
+	return '<b>Hello %s!</b>' % item['name']
+
+bottle.run(host='localhost', port=8082)
+
